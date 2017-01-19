7aa15d8c9cf365b0d5a4670ec21876e43498f33f
Alf Eaton <hubgit@users.noreply.github.com>
2017-01-19 10:50:23 +0000

    Disable validation after generating correction or retraction XML

--- src/PeerJ/Conversion/JATS.php
+++ src/PeerJ/Conversion/JATS.php
@@ -57,7 +57,7 @@ class JATS
     public function generateCorrection(\DOMDocument $input, $params = array())
     {
         $output = $this->convert('jats-to-correction', $input, $params);
-        $this->validateWithDTD($output);
+        //$this->validateWithDTD($output);
 
         return $output;
     }
@@ -73,7 +73,7 @@ class JATS
     public function generateRetraction(\DOMDocument $input, $params = array())
     {
         $output = $this->convert('jats-to-retraction', $input, $params);
-        $this->validateWithDTD($output);
+        //$this->validateWithDTD($output);
 
         return $output;
     }
