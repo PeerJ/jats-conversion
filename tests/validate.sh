649c66e6bd16c229e2399bf746670bf3961091c5
Michael Stoner <michael.stoner@peerj.com>
2019-10-08 19:59:30 +0200

    added crossref 4.4.2

--- tests/validate.sh
+++ tests/validate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 if [ -z "$1" ]
-  then
-    echo "Usage: $0 {article.xml}"
-    exit
+then
+	echo "Usage: $0 {article.xml}"
+	exit
 fi
 
 ARTICLE=$1
@@ -14,6 +13,7 @@ TRACE=''
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 XSL="$DIR/../src/data/xsl"
+XSLT="$XSL/jats-to-unixref.xsl"
 RESOURCES="$DIR/../schema"
 
 OUTPUT_DIR=$(mktemp -d -t validate-XXX)
@@ -24,26 +24,29 @@ export XML_CATALOG_FILES="$RESOURCES/catalog.xml"
 #export SGML_CATALOG_FILES=catalog.xml
 
 echo "Validating against JATS DTD"
-xmllint --loaddtd --valid  --nonet $TRACE --noout --catalogs "$ARTICLE"
+xmllint --loaddtd --valid --nonet $TRACE --noout --catalogs "$ARTICLE"
 
 echo "Validating against JATS XSD"
 # TODO: detect version or add option
 xmllint --nonet $TRACE --noout --catalogs --schema 'http://jats.nlm.nih.gov/publishing/1.1/xsd/JATS-journalpublishing1.xsd' "$ARTICLE"
 
-echo "Validating for CrossRef DOI deposition"
+DEPOSITION="$OUTPUT_DIR/$FILE-CrossRef-DOI-deposition.xml"
+echo "Creating CrossRef DOI deposition - $DEPOSITION"
 xsltproc --catalogs \
-    --nodtdattr \
+	--nodtdattr \
 	--stringparam 'timestamp' `date +"%s"` \
 	--stringparam 'depositorName' 'test' \
 	--stringparam 'depositorEmail' 'test@example.com' \
-	"$XSL/jats-to-unixref.xsl" "$ARTICLE" \
-	| xmllint --nonet $TRACE --noout --schema "$RESOURCES/crossref/crossref4.3.6.xsd" -
+	"$XSLT" "$ARTICLE" > "$DEPOSITION"
+
+echo "Validating CrossRef DOI deposition - $DEPOSITION"
+xmllint --nonet $TRACE --noout --schema "$RESOURCES/crossref/crossref4.4.2.xsd" "$DEPOSITION"
 
 echo "Generating CrossRef schematron report"
 OUTPUT="$OUTPUT_DIR/$FILE-crossref-schematron-report.xml"
-#xsltproc --catalogs --stringparam "timestamp" `date +"%s"` "$$XSL/jats-to-unixref.xsl" "$ARTICLE" | xsltproc "$RESOURCES/schematron/crossref.xsl" -
-xsltproc --catalogs --stringparam "timestamp" `date +"%s"` "$XSL/jats-to-unixref.xsl" "$ARTICLE" > "$OUTPUT_DIR/crossref.xml"
-saxon "$OUTPUT_DIR/crossref.xml" "$RESOURCES/crossref/schematron.xsl" > "$OUTPUT"
+# cat "$DEPOSITION" | xsltproc "$RESOURCES/crossref/schematron.xsl" -
+# note this saxon8.jar is downloaded by the schema folder makefile, and copied here
+java -jar saxon8.jar "$DEPOSITION" "$RESOURCES/crossref/schematron.xsl" > "$OUTPUT"
 echo "CrossRef schematron report written to $OUTPUT"
 
 echo "Checking PMC tagging style"
@@ -55,5 +58,8 @@ echo "Generating HTML"
 OUTPUT="$OUTPUT_DIR/$FILE-preview.html"
 xsltproc --catalogs -output "$OUTPUT" "$XSL/jats-to-html.xsl" "$ARTICLE"
 echo "HTML written to $OUTPUT"
+
+# cp "$DEPOSITION" .
+# cp "$OUTPUT_DIR" .
 
 # TODO: run JS tests in PhantomJS
